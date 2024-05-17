b7386ba9e61037291310895905ec9a9addb36c4f
jdusengimana <jdusengimana@fh.org>
2024-05-16 18:13:17 -0700

    external JSON module fix and JS object

--- index.js
+++ index.js
@@ -2,8 +2,29 @@
 let footerYear = new Date().getFullYear();
 let footerYearElement = document.getElementById("main-text-footer").innerHTML = footerYear;
 
+let textBodyOjectUw = {
+    uw_recomm: "It seems like to are underweight! according to the results, you may consider eating food that contain a reasonable amount of carbohydrate, and make sure you eat a lot of protein, be aware of sodium. Too much sodium is not recommended by FDA.",
+    uw_actve: "Make sure exercise every other week, its always good to exercise to get a good night sleep.",
+    uw_medic: "Regular check ups won’t hurt. You may consider talking to your doctor since your are underweight, this also can be optional as of right now.",
+};
 
+let textBodyOjectHty = {
+    hty_recomm: "You are healthy! Congratulation you should be proud of yourself. Make sure to eat healthy food. Watch out for sugar and sodium intake.",
+    hty_actve: "Make sure you exercise once or twice a week, its always good to exercise to get a good night sleep. Keep up the good work.",
+    hty_medic: "Regular check ups wouldn’t hurt. However there is no need to go see a doctor at this moment. Vitamins should be a good option for you.",
+};
 
+let textBodyOjectOw = {
+    ow_recomm: "You are overweight, Don’t panic. This is normal Make sure to eat more healthy food. Such as salads and fruits. Stay away from sodium and sugar.",
+    ow_actve: "Make sure you exercise regularly, jog, walk or sign up for gym membership. its always good to exercise to get a good night sleep.",
+    ow_medic: "Regular check ups wouldn’t hurt. Although you may consult your doctor to check your blood pressure and your sugar level.",
+};
+
+let textBodyOjectOb = {
+    ob_recomm: "Obesity is a common issue to the western countries, make sure you start fasting right away and only eat salads and fruits.",
+    ob_actve: "Make sure you exercise everyday, jog, walk or sign up for gym membership today. its always good to exercise to get a good night sleep.",
+    ob_medic: "You may consult your doctor to check your blood pressure and your sugar level regularly. Obesity is generally not good for your health.",
+};
 
 //MAIN JS FUNCTION CALLBACK
 function bmiCalculate() {
@@ -21,28 +42,37 @@ function submitChecker() {
         var submitCheckNotNull = document.getElementById("submitData");
         submitCheckNotNull.value = "Reset";
         submitCheckNotNull.setAttribute("onclick", "document.location.reload()");
+        
+        if(bmiCalculateForm) {
+            var heightFeetClear = document.getElementById("feetHeight");
+            var heightInchInitClear = document.getElementById("inchHeight");
+            var weightClear = document.getElementById("weightVar");
+
+            heightFeetClear.disabled = true;
+            heightInchInitClear.disabled = true;
+            weightClear.disabled = true;
+        }
     }
 }
     if(bmiCalculateForm < 18) {
         document.getElementById("result_main_text").innerHTML = "You are underweight";
-        document.getElementById("result-canva--recomm").innerHTML = "123";
-
-        
 
         var canva_uw_element = document.getElementById("main-ref--canva");
         canva_uw_element.classList.add("main-ref--canva-uw");
+        
 
 
             var topRefImg = document.getElementById("topFlexReferrer");
             topRefImg.setAttribute("src", "./assets/uw_food.png");
+            document.getElementById("result-canva--recomm").innerHTML = textBodyOjectUw.uw_recomm;
 
+            var midRefImg = document.getElementById("midFlexReferrer");
+            midRefImg.setAttribute("src", "./assets/lightexercise.png");
+            document.getElementById("result-canva--sub-main-flex-text-active").innerHTML = textBodyOjectUw.uw_actve;
             
-                
-
-                    var midRefImg = document.getElementById("midFlexReferrer");
-                    midRefImg.setAttribute("src", "./assets/lightexercise.png");
-                        var bottomRefImg = document.getElementById("bottomFlexReferrer");
-                        bottomRefImg.setAttribute("src", "./assets/vitamins.png");
+            var bottomRefImg = document.getElementById("bottomFlexReferrer");
+            bottomRefImg.setAttribute("src", "./assets/vitamins.png");
+            document.getElementById("result-canva--sub-main-flex-text-medc").innerHTML = textBodyOjectUw.uw_medic;
         //Call_submit_checker
         submitChecker();
     }
@@ -55,10 +85,16 @@ function submitChecker() {
 
         var topRefImg = document.getElementById("topFlexReferrer");
         topRefImg.setAttribute("src", "./assets/h_food.png");
-            var midRefImg = document.getElementById("midFlexReferrer");
-            midRefImg.setAttribute("src", "./assets/lightexercise.png");
-                var bottomRefImg = document.getElementById("bottomFlexReferrer");
-                bottomRefImg.setAttribute("src", "./assets/vitamins.png");
+        document.getElementById("result-canva--recomm").innerHTML = textBodyOjectHty.hty_recomm;
+
+        var midRefImg = document.getElementById("midFlexReferrer");
+        midRefImg.setAttribute("src", "./assets/lightexercise.png");
+        document.getElementById("result-canva--sub-main-flex-text-active").innerHTML = textBodyOjectHty.hty_actve;
+
+        var bottomRefImg = document.getElementById("bottomFlexReferrer");
+        bottomRefImg.setAttribute("src", "./assets/vitamins.png");
+        document.getElementById("result-canva--sub-main-flex-text-medc").innerHTML = textBodyOjectHty.hty_medic;
+
         //Call_submit_checker
         submitChecker();
     }
@@ -71,10 +107,15 @@ function submitChecker() {
 
         var topRefImg = document.getElementById("topFlexReferrer");
         topRefImg.setAttribute("src", "./assets/ow_food.png");
-            var midRefImg = document.getElementById("midFlexReferrer");
-            midRefImg.setAttribute("src", "./assets/ow_exercise.png");
-                var bottomRefImg = document.getElementById("bottomFlexReferrer");
-                bottomRefImg.setAttribute("src", "./assets/ow_health.png");
+        document.getElementById("result-canva--recomm").innerHTML = textBodyOjectOw.ow_recomm;
+
+        var midRefImg = document.getElementById("midFlexReferrer");
+        midRefImg.setAttribute("src", "./assets/ow_exercise.png");
+        document.getElementById("result-canva--sub-main-flex-text-active").innerHTML = textBodyOjectOw.ow_actve;
+
+        var bottomRefImg = document.getElementById("bottomFlexReferrer");
+        bottomRefImg.setAttribute("src", "./assets/ow_health.png");
+        document.getElementById("result-canva--sub-main-flex-text-medc").innerHTML = textBodyOjectOw.ow_medic;
         //Call_submit_checker
         submitChecker();
     }
@@ -87,10 +128,15 @@ function submitChecker() {
 
         var topRefImg = document.getElementById("topFlexReferrer");
         topRefImg.setAttribute("src", "./assets/ob_food.jpg");
-            var midRefImg = document.getElementById("midFlexReferrer");
-            midRefImg.setAttribute("src", "./assets/ob_exercise.png");
-                var bottomRefImg = document.getElementById("bottomFlexReferrer");
-                bottomRefImg.setAttribute("src", "./assets/ob_health.png");
+        document.getElementById("result-canva--recomm").innerHTML = textBodyOjectOb.ob_recomm;
+        
+        var midRefImg = document.getElementById("midFlexReferrer");
+        midRefImg.setAttribute("src", "./assets/ob_exercise.png");
+        document.getElementById("result-canva--sub-main-flex-text-active").innerHTML = textBodyOjectOb.ob_actve;
+                
+        var bottomRefImg = document.getElementById("bottomFlexReferrer");
+        bottomRefImg.setAttribute("src", "./assets/ob_health.png");
+        document.getElementById("result-canva--sub-main-flex-text-medc").innerHTML = textBodyOjectOb.ob_medic;
         //Call_submit_checker
         submitChecker()
     }
@@ -128,6 +174,7 @@ function submitChecker() {
               //Export-JSON
               document.getElementById("textOutTitle").innerHTML = item.tipTitle;
               document.getElementById("textOutParaph").innerHTML = item.tipBody;
+              document.getElementById("textOutParaphjson").innerHTML = "-external JSON datasource-";
             } else {
               console.error('Index is out of bounds');
             }
